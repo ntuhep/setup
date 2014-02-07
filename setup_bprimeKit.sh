cf1851655beec49ca7450766da70ed6be3033afb
Ulysses Grundler <grundler@cern.ch>
2014-02-07 16:39:14 +0100

    Minimum requirements to compile, after moving HitFit related code out

--- setup_bprimeKit.sh
+++ setup_bprimeKit.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
+#Set up the CMSSW envirnoment
 scram project CMSSW_5_3_14_patch2
 cd CMSSW_5_3_14_patch2/src
 cmsenv
-git cms-addpkg FWCore/Version
+git cms-addpkg FWCore/Version #This should change at some point to git cms-init 
+
+#This is the center piece. Check out bprimeKit code
 git clone https://github.com/ntuhep/bprimeKit.git MyAna/bprimeKit
 
-# git cms-addpkg EgammaAnalysis/ElectronTools
-# cd EgammaAnalysis/ElectronTools/data/
-# cat download.url | xargs wget
-# cd -
-
+#UserCode that is required
+#We should evaluate whether we truly need this, if so, perhaps it should be in official CMSSW code
 git clone https://github.com/ETHZ/sixie-Muon-MuonAnalysisTools.git UserCode/sixie/Muon/MuonAnalysisTools
-
-git clone https://github.com/ntuhep/bpkHitFit.git MyAna/bpkHitFit
-
 git clone https://github.com/amarini/QuarkGluonTagger.git
 
+#Compile everything
 scramv1 b -j 20
