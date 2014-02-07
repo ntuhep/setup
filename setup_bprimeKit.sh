bfef75203b0a98cf7b4c06fd7f8f31efe62f9b0e
Ulysses Grundler <grundler@cern.ch>
2014-02-07 17:09:12 +0100

    Allow option to include bpkHitFit and bpkHitFitAnalysis in installation

--- setup_bprimeKit.sh
+++ setup_bprimeKit.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+echo "Beginning download and compilation of bprimeKit package"
+
+installHitFit=false
+
+#Allow for some command line arguments
+while test $# -gt 0; do
+	case $1 in
+		--hitfit)
+			installHitFit=true
+			echo "  Include HitFit packages"
+			shift
+			;;
+		*)
+			echo >&2 "  Invalid argument: $1"
+			;;
+	esac
+	shift
+done
 
 #Set up the CMSSW envirnoment
 scram project CMSSW_5_3_14_patch2
@@ -13,6 +31,12 @@ git clone https://github.com/ntuhep/bprimeKit.git MyAna/bprimeKit
 #We should evaluate whether we truly need this, if so, perhaps it should be in official CMSSW code
 git clone https://github.com/ETHZ/sixie-Muon-MuonAnalysisTools.git UserCode/sixie/Muon/MuonAnalysisTools
 git clone https://github.com/amarini/QuarkGluonTagger.git
+
+#Add HitFit related code if requested
+if $installHitFit; then
+	git clone https://github.com/ntuhep/bpkHitFit.git MyAna/bpkHitFit
+	git clone https://github.com/ntuhep/bpkHitFitAnalysis.git MyAna/bpkHitFitAnalysis
+fi
 
 #Compile everything
 scramv1 b -j 20
