8664f674bc4fec94f2034520b4b817aa79d7b911
Ulysses Grundler <grundler@cern.ch>
2014-08-28 14:56:19 +0200

    Made CMSSW version changeable, added option to not compile immediately, and return if invalid option given

--- setup_bprimeKit.sh
+++ setup_bprimeKit.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 echo "Beginning download and compilation of bprimeKit package"
 
+CMSSW_VERSION="CMSSW_5_3_14_patch2"
 GITHUB_PREFIX="https://github.com/"
 INSTALL_HITFIT=false
+COMPILE=true
 
 #Allow for some command line arguments
 while test $# -gt 0; do
 	case $1 in
+		CMSSW*)
+			CMSSW_VERSION=$1
+			echo "  Using $CMSSW_VERSION"
+			shift
+			;;
 		--ssh)
 			GITHUB_PREFIX="git@github.com:"
 			echo "  Using SSH instead of HTTPS"
@@ -17,8 +24,19 @@ while test $# -gt 0; do
 			echo "  Include HitFit packages"
 			shift
 			;;
+		--no-compile)
+			COMPILE=false
+			echo "  do not compile"
+			shift
+			;;
+		autolist) #seems to happen when no argument given
+			shift
+			;;
 		*)
 			echo >&2 "  Invalid argument: $1"
+			echo >&2 "    QUITTING"
+			shift
+			return
 			;;
 	esac
 done
@@ -28,8 +46,9 @@ done
 #########################################
 
 #Set up the CMSSW envirnoment
-scram project CMSSW_5_3_14_patch2
-cd CMSSW_5_3_14_patch2/src
+scram project $CMSSW_VERSION
+cd $CMSSW_VERSION/src
+
 cmsenv
 git cms-addpkg FWCore/Version #This should change at some point to git cms-init 
 
@@ -48,4 +67,8 @@ if $INSTALL_HITFIT; then
 fi
 
 #Compile everything
-scramv1 b -j 20
+if $COMPILE; then
+	scramv1 b -j 20
+fi
+
+cd -
